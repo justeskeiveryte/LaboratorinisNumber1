bb0da6a65daac10d8719b8f691d72bae56a5c684
justeskeiv <juste.skeiveryte@gmail.com>
2022-02-27 20:58:22 +0200

    Added change Text Color functionality

--- app/src/main/java/com/example/laboratorinisnumber1/MainActivity.java
+++ app/src/main/java/com/example/laboratorinisnumber1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.laboratorinisnumber1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView tvMain;
+    TextView tvMainColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +18,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.tvMain = findViewById(R.id.tvMain);
+        this.tvMainColor = findViewById(R.id.txtChangeColor);
     }
 
     public void OnBtnclick(View view) {
         this.tvMain.setText(R.string.changed_label_caption);
     }
-}
+
+    public void OnBtnclickChangeColour(View view) {
+        this.tvMainColor.setTextColor(Color.RED);
+    }
+};
+
+
